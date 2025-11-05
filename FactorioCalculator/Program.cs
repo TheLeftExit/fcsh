2fcc5a15af470dac5e1fd2a8b2a9f55b8be30ddb
TheLeftExit <norton@theleftexit.net>
2025-11-05 23:35:14 +0200

    better lua dumper

--- FactorioCalculator/Program.cs
+++ FactorioCalculator/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Reflection;
+using System.Text.Json;
+
+using(var jsonStream = File.OpenRead("data.json"))
+{
+    var root = JsonSerializer.Deserialize<DataDumperRoot>(jsonStream, new JsonSerializerOptions()
+    {
+        AllowTrailingCommas = true
+    });
+
+    ;
+}
+
+
+public class DataDumperRoot
+{
+    public AnyPrototype[] Recipes { get; init; } = null!;
+    public AnyPrototype[] CraftingMachines { get; init; } = null!;
+}
+
+public class AnyPrototype
+{
+    public string Name { get; init; } = null!;
+}
--- fcsh-data-dumper/data-final-fixes.lua
+++ fcsh-data-dumper/data-final-fixes.lua
@@ -1,5 +1,3 @@
-log("Hello from fcsh-data-dumper!")
-
 -- utils start
 
 local lines = { }
@@ -13,11 +11,15 @@ function writeLine(line)
 end
 
 function writePropertyValue(propertyName, propertyValue)
-    writeLine('"' .. propertyName .. '": ' .. propertyValue .. ',')
+    writeLine('"' .. propertyName .. '": ' .. (propertyValue or 'null') .. ',')
 end
 
 function writePropertyString(propertyName, propertyValue)
-    writePropertyValue(propertyName, '"' .. propertyValue .. '"')
+    if(propertyValue == nil) then
+        writePropertyValue(propertyName, nil)
+    else
+        writeLine('"' .. propertyName .. '": "' .. propertyValue .. '",')
+    end
 end
 
 function writePropertyObject(propertyName)
@@ -40,9 +42,26 @@ function writeObjectEnd()
     writeLine('},')
 end
 
+function writeObjectEndFinal()
+    indentDec()
+    writeLine('}')
+end
+
 function writeArrayEnd()
     indentDec()
     writeLine('],')
+end
+
+function writeItemValue(itemValue)
+    writeLine((itemValue or 'null') .. ',')
+end
+
+function writeItemString(itemValue)
+    if(itemValue == nil) then
+        writeItemValue(nil)
+    else
+        writeLine('"' .. itemValue .. '",')
+    end
 end
 
 -- utils end
@@ -51,18 +70,95 @@ end
 writeLine('')
 writeLine('fcsh-data-dumper-start')
 
-writeObjectStart()
+-- json start
+
+writeObjectStart() -- https://lua-api.factorio.com/latest/types/Data.html
 
 writePropertyArray('Recipes')
-for k, v in pairs(data.raw['recipe']) do
-    writeObjectStart()
-    writePropertyString('Name', k)
+for _, recipe in pairs(data.raw['recipe']) do
+    writeObjectStart() -- https://lua-api.factorio.com/latest/prototypes/RecipePrototype.html
+    writePropertyString('Name', recipe.name)
+    writePropertyValue('EnergyRequired', recipe.energy_required or 0.5)
+    writePropertyString('Category', recipe.category or 'crafting')
+
+    if(recipe.ingredients ~= nil) then
+        writePropertyArray('Ingredients')
+        for _, ingredient in pairs(recipe.ingredients) do
+            writeObjectStart() -- https://lua-api.factorio.com/latest/types/IngredientPrototype.html
+            writePropertyString('Name', ingredient.name)
+            writePropertyString('Type', ingredient.type)
+            writePropertyValue('Amount', ingredient.amount)
+            if(ingredient.type == 'fluid') then
+                writePropertyValue('Temperature', ingredient.temperature)
+                writePropertyValue('MinimumTemperature', ingredient.minimum_temperature)
+                writePropertyValue('MaximumTemperature', ingredient.maximum_temperature)
+            end
+            writeObjectEnd()
+        end
+        writeArrayEnd()
+    end
+
+    if(recipe.results ~= nil) then
+        writePropertyArray('Results')
+        for _, product in pairs(recipe.results) do
+            writeObjectStart() -- https://lua-api.factorio.com/latest/types/ProductPrototype.html
+            writePropertyString('Name', product.name)
+            writePropertyString('Type', product.type)
+            writePropertyValue('Amount', product.amount)
+            writePropertyValue('AmountMin', product.amount_min)
+            writePropertyValue('AmountMax', product.amount_max)
+            writePropertyValue('Probability', product.probability)
+            if(product.type == 'item') then
+                writePropertyValue('ExtraCountFraction', product.extra_count_fraction)
+            else
+                writePropertyValue('Temperature', product.temperature)
+            end
+            writeObjectEnd()
+        end
+        writeArrayEnd()
+    end
+
     writeObjectEnd()
 end
 writeArrayEnd()
 
-writeObjectEnd()
+writePropertyArray('CraftingMachines')
+for _, craftingMachineType in pairs({ 'assembling-machine', 'rocket-silo', 'furnace' }) do
+    for _, craftingMachine in pairs(data.raw[craftingMachineType]) do
+        writeObjectStart() -- https://lua-api.factorio.com/latest/prototypes/CraftingMachinePrototype.html
+        writePropertyString('Name', craftingMachine.name)
+        writePropertyValue('CraftingSpeed', craftingMachine.crafting_speed)
+
+        writePropertyArray('CraftingCategories')
+        for _, craftingCategory in pairs(craftingMachine.crafting_categories) do
+            writeItemString(craftingCategory)
+        end
+        writeArrayEnd()
+
+        if(craftingMachine.effect_receiver ~= nil) then
+            writePropertyObject('EffectReceiver')
+            if(craftingMachine.effect_receiver.base_effect ~= nil) then
+                writePropertyObject('BaseEffect')
+                
+                writePropertyValue('Consumption', craftingMachine.effect_receiver.base_effect.consumption)
+                writePropertyValue('Speed', craftingMachine.effect_receiver.base_effect.speed)
+                writePropertyValue('Productivity', craftingMachine.effect_receiver.base_effect.productivity)
+                writePropertyValue('Pollution', craftingMachine.effect_receiver.base_effect.pollution)
+                writePropertyValue('Quality', craftingMachine.effect_receiver.base_effect.quality)
+
+                writeObjectEnd()
+            end
+            writeObjectEnd()
+        end
+
+        writeObjectEnd()
+    end
+end
+writeArrayEnd()
+
+writeObjectEndFinal()
+
+-- json end
 
 writeLine('fcsh-data-dumper-end')
-
 log(table.concat(lines, '\n'))
